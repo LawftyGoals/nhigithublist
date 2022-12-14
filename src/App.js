1323dbbd35af49cacc386c6dba8463b293b6c171
Oliver Sokol <oliver.sokol@atea.no>
2022-12-14 11:43:44 +0100

    data collection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+
 
 
 
 const App = (props) => {
+const [dataList, setDataList] = useState();
+
+useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.github.com/users?since=0&per_page=20');
+      const data = await response.json();
+
+      setDataList(data);
+    };
+
+    fetchData();
+  }, []
+);
   return(
     <div>
-
+      <div>{props.title}</div>
+      <div>{dataList[0].login}</div>
     </div>
   );
 }
