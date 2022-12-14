7df0b66073c533d33b816c9e920376e0a4d93353
Oliver Sokol <oliver.sokol@atea.no>
2022-12-14 11:48:31 +0100

    move datacollection to own object

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import React, {useState, useEffect} from "react";
 
 
 
-const App = (props) => {
-const [dataList, setDataList] = useState();
+const DataFetcher = props => {
 
-useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://api.github.com/users?since=0&per_page=20');
-      const data = await response.json();
+  useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch('https://api.github.com/users?since=0&per_page=20');
+        const data = await response.json();
 
-      setDataList(data);
-    };
+        props.setDataList(data);
+      };
 
-    fetchData();
-  }, []
-);
-  return(
-    <div>
-      <div>{props.title}</div>
-      <div>{dataList[0].login}</div>
-    </div>
+      fetchData();
+    }, []
   );
+}
+
+const App = (props) => {
+  const [dataList, setDataList] = useState();
+  
+    return(
+      <div>
+        <DataFetcher setDataList={setDataList}/>
+        <div>{props.title}</div>
+        <div>{dataList[0].login}</div>
+      </div>
+    );
 }
 
 export default App;
